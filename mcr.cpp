8841316220ca9849fd2c530c19e12ba0680ba619
Susie <susie30@foxmail.com>
2021-09-06 21:48:11 +0800

    Update mcr.cpp
    test: add tab

--- mcr.cpp
+++ mcr.cpp
@@ -24,8 +24,8 @@ int main(){
 	for (int n=0; n<9; n++){
 	    turn = !turn;  // use the not-operator to change true to false or false to true.    
 	    if (turn == false)
-		    cout << "Player 1: ";
-    	else
+                cout << "Player 1: ";
+    	    else
     		cout << "Player 2: ";
     	
 		cout << "Which cell to mark? i:[1..3], j:[1..3]: "; 
